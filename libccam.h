406786f02963f17241cbf4c3d02f72c1ede086a8
BogdanTheGeek <bogdan.ionescu.work@gmail.com>
2021-02-25 09:48:53 +0000

    initial commit

---
+++ libccam.h
@@ -0,0 +1,37 @@
+/*
+MIT License
+
+Copyright (c) 2021 Bogdan Ionescu
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+
+typedef struct point{
+	double x;
+	double y;
+	double z;
+};
+
+void move(double x, double y, double z);	//incremental move with cutting feedrate
+void travel(double x, double y, double z);	//absolute move with travel feedrate
+
+void bore(double x, double y, double z, double diameter, double pitch, double depth);	//bore a hole of the specified dimensions
+void drill(point *p, int len, double depth);	//drill all points in array
+void peck(point *p, int len, double depth, double pecks, double ratio);	//drill all points in array by pecking n times, the depth before a retract is controlled by ratio
