ce13e97834a4425f3c9a1fbaefd729742f486dbc
BogdanTheGeek <bogdan.ionescu.work@gmail.com>
2021-03-01 14:40:12 +0000

    [CLEAN] fixed some examples and added pictures and expected gcode for each example

--- examples/cutout.c
+++ examples/cutout.c
@@ -3,19 +3,41 @@
 point square[4] = {{1,0},{1,1},{0,1},{0,0}};
 point relative_square[4] = {{1,0},{0,1},{-1,0},{0,-1}};
 
+point contour[4] = {{40,-10,-3},{40,20,-3},{-10,20,-3},{-10,-10,-3}};
+
 int main(){
 	//start of program
 	set_feed(100);
 	set_rapid(1000);
 	
-	profile(square, 4, false);	
+	move(0,0,-1, REL);
+	profile(square, 4, ABS);	
+	move(0,0,1, REL);
 	
 	travel(10,10,0, false);
-	profile(relative_square, 4, true);	
+	
+	move(0,0,-1, REL);
+	profile(relative_square, 4, REL);	
+	move(0,0,1, REL);
+	
 	travel(20,10,0, false);
-	profile(relative_square, 4, true);	
+	
+	move(0,0,-1, REL);
+	profile(relative_square, 4, REL);	
+	move(0,0,1, REL);
+	
 	travel(30,10,0, false);
-	profile(relative_square, 4, true);	
+
+	move(0,0,-1, REL);
+	profile(relative_square, 4, REL);	
+	move(0,0,1, REL);
+
+	travel(-10,-10,0, ABS);
+
+	move(0,0,-3, REL);
+	profile(contour, 4, ABS);	
+	move(0,0,3, REL);
+	
 
 	//end of program
 	stop();
--- examples/face.c
+++ examples/face.c
@@ -6,6 +6,7 @@ int main(){
 	set_rapid(1000);
 
 	travel(10,20,0,ABS);
+	move(0,0,-1, REL);
 	face(20,10,1,60);	// all values must be positive
 	//end of program
 	stop();
